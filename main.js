fcd8fbfae9fe433a79401b6b4b565a07b2157b86
devjaq <devjaq@gmail.com>
2019-05-24 10:40:39 -0400

    adjustments to style and improvements to functionality

--- main.js
+++ main.js
@@ -6,82 +6,143 @@ function showError(msg) {
 
 // Searches for books and returns a promise that resolves a JSON list
 function searchForBooks(term) {
-  let searchResults;
+  resultsArea = document.getElementById('results-area');
+
+  if (document.getElementById('top-results')) {
+
+  } else {
+    topResults = document.createElement('p');
+    topResults.id = 'top-results';
+  }
+
+  resultsArea.prepend(topResults);
+
   let searchBox = document.getElementById('search-bar');
+
+  if (searchBox.value === "" || searchBox.value === undefined) {
+    if (term === undefined) {
+      term = randomFirstTopic();
+    }
+  } else {
+  }
+
   // TODO fetch data
-  fetch(`https://www.googleapis.com/books/v1/volumes?q={${searchBox.value}}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      searchResults = myJson;
-      render(searchResults);
-    });
+  if (searchBox.value == "" || searchBox.value == undefined) {
+    // topResults.innerText = `Top results for "${term}".`;
+    // topResults.id = `top-results-${term}`;
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q={${term}}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        searchResults = myJson;
+        render(searchResults);
+        topResults.innerText = `Top ${searchResults.items.length} results for "${term}".`;
+      });
+  } else {
+    // topResults.id = `top-results-${searchBox.value}`;
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q={${searchBox.value}}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        searchResults = myJson;
+        render(searchResults);
+        topResults.innerText = `Top ${searchResults.items.length} results for "${searchBox.value}".`;
+      });
+  }
+  // fetch(`https://www.googleapis.com/books/v1/volumes?q={${searchBox.value}}`)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (myJson) {
+  //     searchResults = myJson;
+  //     render(searchResults);
+  //   });
+}
+
+const keypress = (event) => {
+  if (event.key === "Enter") {
+    searchForBooks();
+  }
 }
 
 // Generate HTML and sets #results's contents to it
 function render(searchResults) {
   let resultsItem;
-  // TODO display results
-  // console.log(searchResults.items);
+  let bookData = searchResults.items;
 
+  // TODO display results
   let resultsList = document.getElementById('results');
 
-  for (let i = 0; i < searchResults.items.length; i++) {
-    console.log(searchResults.items[i]);
-    console.log(searchResults.items[1].id);
-    
-    if (document.getElementById(searchResults.items[i].id)) {
-    } else {
+  ////// CLEAR OUT RESULTS LIST
+  // resultsList.innerHTML = "";
+  //////
+
+  ///// Turn the book data into cards
+  for (let i = 0; i < bookData.length; i++) {
+    // console.log(bookData[i]);
+    // console.log(bookData[1].id);
+
+    if (document.getElementById(bookData[i].id)) {
+      //// delete old card, create new one
+      document.getElementById(bookData[i].id).outerHTML = "";
+
+    } 
+    // else {
       resultsItem = document.createElement('li');
       resultsCard = document.createElement('section');
       resultsCard.classList.add('book');
-      resultsCard.id = searchResults.items[i].id;
+      resultsCard.id = bookData[i].id;
       cardInfo = document.createElement('div');
       cardInfo.classList.add('info');
       resultsCard.appendChild(cardInfo);
 
       //// Create all sub elements for card
       cardImage = document.createElement('img');
-      if (searchResults.items[i].volumeInfo.imageLinks) {
-        cardImage.src = searchResults.items[i].volumeInfo.imageLinks.thumbnail;
-        cardImage.style.height = cardImage.height;
-        resultsCard.style.height = searchResults.items[i].volumeInfo.imageLinks.thumbnail.height;
+      if (bookData[i].volumeInfo.imageLinks) {
+        // cardImage = document.createElement('div');
+        cardImage.src = bookData[i].volumeInfo.imageLinks.thumbnail;
+        cardImage.style.backgroundImage = `url(${bookData[i].volumeInfo.imageLinks.thumbnail})`;
+        resultsCard.style.height = bookData[i].volumeInfo.imageLinks.thumbnail.height;
       } else {
+        cardImage = document.createElement('div');
         cardImage.classList.add('blank');
       }
+      cardInfoTitle = document.createElement('h2');
+      cardInfoTitle.innerText = bookData[i].volumeInfo.title;
+
       cardTitle = document.createElement('h2');
-      cardTitle.innerText = searchResults.items[i].volumeInfo.title.toUpperCase();
-      cardTitle1 = document.createElement('h2');
-      cardTitle1.innerText = searchResults.items[i].volumeInfo.title.toUpperCase();
+      cardTitle.innerText = bookData[i].volumeInfo.title.toUpperCase();
 
       cardSubtitle = document.createElement('h3');
-      if (searchResults.items[i].volumeInfo.subtitle === undefined) {
+      if (bookData[i].volumeInfo.subtitle === undefined) {
         cardSubtitle.innerText = "";
       } else {
-        cardSubtitle.innerText = searchResults.items[i].volumeInfo.subtitle;
+        cardSubtitle.innerText = bookData[i].volumeInfo.subtitle;
       }
       cardAuthors = document.createElement('p');
-      if (searchResults.items[i].volumeInfo.authors === undefined) {
+      if (bookData[i].volumeInfo.authors === undefined) {
         cardAuthors.innerText = "Author: N/A";
       } else {
-        cardAuthors.innerText = searchResults.items[i].volumeInfo.authors;
+        cardAuthors.innerText = bookData[i].volumeInfo.authors;
       }
       cardLink = document.createElement('a');
-      cardLink.href = searchResults.items[i].volumeInfo.infoLink;
+      cardLink.href = bookData[i].volumeInfo.infoLink;
 
       //// Add all sub elements to card
       cardLink.appendChild(resultsCard);
-      resultsCard.appendChild(cardTitle1);
-      cardInfo.appendChild(cardTitle);
+      resultsCard.appendChild(cardTitle);
       resultsCard.appendChild(cardImage);
+      cardInfo.appendChild(cardInfoTitle);
       cardInfo.appendChild(cardSubtitle);
       cardInfo.appendChild(cardAuthors);
       resultsItem.appendChild(cardLink);
       resultsList.prepend(resultsItem);
-    }
+    // }
   }
-
 }
 
 function focusEffect() {
@@ -94,4 +155,11 @@ function blurEffect() {
   searchBar.placeholder = "Input search term here";
 }
 
-searchForBooks();
+const randomFirstTopic = () => {
+  let topics = [
+    "doctor", "networking", "health", "record", "school", "nursing", "camp"
+  ]
+  return topics[Math.floor(Math.random() * topics.length)];
+}
+
+searchForBooks(randomFirstTopic());
